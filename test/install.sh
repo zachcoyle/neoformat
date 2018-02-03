325fbd735bb6517552e85db797427e8986a969e1
Steve Dignam <steve@dignam.xyz>
2018-02-02 22:06:35 -0500

    f

--- test/install.sh
+++ test/install.sh
@@ -43,4 +43,6 @@ if [ ! -d "$HOME/.vim/plugged/vader.vim" ]; then
 fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export PATH="$DIR/bin":$PATH
+export $PATH="$DIR/bin":$PATH
+
+cp bin/exit64 usr/bin
